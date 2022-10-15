be1e376af1de873f327a673df2dbe6dedd726e84
django2.2p3.7 <zenakorneev940@gmail.com>
2022-10-15 17:26:27 +0300

    modul 7

--- 05_Forms/news/app_news/admin.py
+++ 05_Forms/news/app_news/admin.py
@@ -2,17 +2,46 @@ from django.contrib import admin
 from .models import News, Comment
 
 
+def make_published(modeladmin, request, queryset):
+    queryset.update(is_published=True)
+
+
+make_published.short_description = "Mark selected stories as published"
+
+
+def move_to_draft(modeladmin, request, queryset):
+    queryset.update(is_published=False)
+
+
+move_to_draft.short_description = "Mark selected stories as draft"
+
+
+def remove_comment(modeladmin, request, queryset):
+    queryset.update(description='Удалено администратором')
+
+
+move_to_draft.short_description = "Delete Comments"
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description', 'date_create', 'date_update', 'is_published')
+    inlines = [CommentInline, ]
+    list_display = ('id', 'title', 'date_create', 'date_update', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'description')
-    list_filter = ('title',)
+    list_filter = ('is_published',)
+    actions = [make_published, move_to_draft]
 
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'author', 'get_description', 'news')
     list_display_links = ('id', 'author')
     search_fields = ('author',)
+    list_filter = ('author',)
+    actions = [remove_comment]
 
 
 admin.site.register(News, NewsAdmin)
